82b4f400ae8391fd538d1c747b2b2dae0fb0938a
CODE CRAFT web sistem <76017481+CODE-CRAFT-WEB-SISTEM@users.noreply.github.com>
2024-04-29 20:06:53 -0300

    Update New functions Copy and Delete

--- main.js
+++ main.js
@@ -2,14 +2,52 @@ const nmA = document.getElementById("nmA");
 const nmB = document.getElementById("nmB");
 const sum = document.getElementById("sum");
 const error = document.getElementById("error");
-
 const closeError = document.getElementById("closeError");
+const options = document.getElementById("options");
 
 const soma = document.getElementById("soma");
 const subtracao = document.getElementById("subtracao");
 const multiplicacao = document.getElementById("multiplicacao");
 const divisao = document.getElementById("divisao");
 
+//Função de limpar campos
+function ClearFields() {
+    document.getElementById("btnDelete").addEventListener("click", () => {
+        nmA.value = "";
+        nmB.value = "";
+        nmA.focus();
+    });
+}
+
+//Função de copiar resultados
+function CopyList(numA, numB) {
+    document.getElementById("btnCopy").addEventListener("click", () => {
+        const listCopy = `
+=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=
+📄 Resultados do cálculos de ${numA} e ${numB}: 
+      
+Soma: ${numA + numB}
+Subtração: ${numA - numB}
+Multiplicação: ${numA * numB}
+Divisão: ${numA / numB}
+=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=
+`;
+
+        navigator.clipboard
+            .writeText(listCopy)
+            .then(() => {
+                alert("Copiado com sucesso!");
+            })
+            .catch(err => {
+                console.error(
+                    "Erro ao copiar para a área de transferência:",
+                    err
+                );
+            });
+    });
+}
+
+//Função de realizar operações
 sum.addEventListener("click", ev => {
     ev.preventDefault();
 
@@ -24,6 +62,10 @@ sum.addEventListener("click", ev => {
         subtracao.textContent = C - D;
         multiplicacao.textContent = C * D;
         divisao.textContent = C / D;
+        options.classList.add("func");
+
+        CopyList(C, D);
+        ClearFields();
     } else {
         error.classList.add("alert");
         nmA.focus();
