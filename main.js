5e6db762e74013364134d624adf0f942c09f379d
CODE CRAFT web sistem <76017481+CODE-CRAFT-WEB-SISTEM@users.noreply.github.com>
2024-04-29 21:45:38 -0300

    update main.js

--- main.js
+++ main.js
@@ -12,21 +12,21 @@ const divisao = document.getElementById("divisao");
 
 //Função de limpar campos
 function ClearFields() {
-    document.getElementById("btnDelete").addEventListener("click", () => {
-        nmA.value = "";
-        nmB.value = "";
-        soma.textContent = "_/_";
-        subtracao.textContent = "_/_";
-        multiplicacao.textContent = "_/_";
-        divisao.textContent = "_/_";
-        nmA.focus();
-    });
+  document.getElementById("btnDelete").addEventListener("click", () => {
+    nmA.value = "";
+    nmB.value = "";
+    soma.textContent = "_/_";
+    subtracao.textContent = "_/_";
+    multiplicacao.textContent = "_/_";
+    divisao.textContent = "_/_";
+    nmA.focus();
+  });
 }
 
 //Função de copiar resultados
 function CopyList(numA, numB) {
-    document.getElementById("btnCopy").addEventListener("click", () => {
-        const listCopy = `
+  document.getElementById("btnCopy").addEventListener("click", () => {
+    const listCopy = `
 📄 *Resultado dos cálculos de ${numA} e ${numB}:* 
 
 =×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=×=
@@ -42,48 +42,47 @@ function CopyList(numA, numB) {
 ☆ _Developed by CodeCraft_
 `;
 
-        navigator.clipboard
-            .writeText(listCopy)
-            .then(() => {
-                alert("Copiado com sucesso!");
-            })
-            .catch(err => {
-                console.error(
-                    "Erro ao copiar para a área de transferência:",
-                    err
-                );
-            });
-    });
+    navigator.clipboard.writeText(listCopy)
+      .then(() => {
+        console.log("Copiado com sucesso!");
+      })
+      .catch(err => {
+        console.error(
+          "Erro ao copiar para a área de transferência:",
+          err
+        );
+      });
+  });
 }
 
 //Função de realizar operações
 sum.addEventListener("click", ev => {
-    ev.preventDefault();
+  ev.preventDefault();
 
-    let A = parseFloat(nmA.value);
-    let B = parseFloat(nmB.value);
+  let A = parseFloat(nmA.value);
+  let B = parseFloat(nmB.value);
 
-    if (!isNaN(A) && !isNaN(B)) {
-        const C = parseInt(A);
-        const D = parseInt(B);
+  if (!isNaN(A) && !isNaN(B)) {
+    const C = parseInt(A);
+    const D = parseInt(B);
 
-        soma.textContent = C + D;
-        subtracao.textContent = C - D;
-        multiplicacao.textContent = C * D;
-        divisao.textContent = C / D;
-        options.classList.add("func");
+    soma.textContent = C + D;
+    subtracao.textContent = C - D;
+    multiplicacao.textContent = C * D;
+    divisao.textContent = C / D;
+    options.classList.add("func");
 
-        CopyList(C, D);
-        ClearFields();
-    } else {
-        error.classList.add("alert");
-        nmA.focus();
-        closeError.addEventListener("click", () => {
-            error.classList.remove("alert");
-        });
+    CopyList(C, D);
+    ClearFields();
+  } else {
+    error.classList.add("alert");
+    nmA.focus();
+    closeError.addEventListener("click", () => {
+      error.classList.remove("alert");
+    });
 
-        setInterval(() => {
-            error.classList.remove("alert");
-        }, 1000 * 8);
-    }
+    setInterval(() => {
+      error.classList.remove("alert");
+    }, 1000 * 8);
+  }
 });
